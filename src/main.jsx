e15c44fd9f5204850c27045736cb8847a6f85941
The Mrinal !! <crypto18089@gmail.com>
2026-05-20 10:02:24 +0530

    main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ChainCFO from './ChainCFO'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChainCFO />
+  </React.StrictMode>
+)
